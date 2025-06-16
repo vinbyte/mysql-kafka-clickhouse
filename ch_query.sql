fa3513bdac6a4d34409c819fb478c2342e33646e
Gavinda Kinandana <hai@gavinda.dev>
2025-06-16 22:32:15 +0700

    update query ch

--- ch_query.sql
+++ ch_query.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS default.departments
     __deleted Bool,
     __source_ts_ms Int64
 )
-ENGINE = ReplacingMergeTree()
+ENGINE = ReplacingMergeTree(__source_ts_ms, __deleted)
 PRIMARY KEY dept_no
 ORDER BY dept_no
--- to drop the row with __deleted = true, manually run : OPTIMIZE TABLE default.departments FINAL CLEANUP;
+-- if the data is duplicated, run this query to perform de-duplication : OPTIMIZE TABLE default.departments FINAL CLEANUP;
 SETTINGS allow_experimental_replacing_merge_with_cleanup=1;
